9752b82d15995f08e11dd2c4a9bb9424968d9f6f
Jatin Dhiman <dhimanjatin003@gmail.com>
2025-07-06 17:01:46 +0530

    button

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,13 +1,6 @@
-import { Link } from "react-router-dom"
 
-export function BottomWarning({label, buttonText, to}) {
-    return <div className="py-2 text-sm flex justify-center">
-      <div>
-        {label}
-      </div>
-      <Link className="pointer underline pl-1 cursor-pointer" to={to}>
-        {buttonText}
-      </Link>
-    </div>
+
+export function Button({label, onClick}) {
+    return <button onClick={onClick} type="button" className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">{label}</button>
 }
   
